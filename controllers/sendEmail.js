1b83e5737e51e0bc27ba6751e5d0912cda45b711
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-21 18:38:02 +0100

    feat: implement email sending functionality in sendEmail controller

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,26 @@
+const nodemailer  = require('nodemailer')
+
+
 const sendEmail = async (req, res) => {
-    res.send('send email func')
+    let testAccount = await nodemailer.createTestAccount()
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "orie.mann18@ethereal.email",
+        pass: "hWF9mJ66hDFqJ2t4At",
+      },
+    });
+
+ let info = await transporter.sendMail({
+    from: '"Coding Addict  " <shem4soul@gmail.com>',
+    to: 'smartresearchers82@gmail.com',
+    subject: "Hello",
+    html: '<h2>Sending Email with Node.js</h2>'
+ })
+
+     res.json(info)
     
 }
 
